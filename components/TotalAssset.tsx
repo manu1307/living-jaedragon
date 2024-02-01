7da49f00fc916c175872d1127eed952be5662441
Sung Chul Park <jasonsc@naver.com>
2024-02-01 11:24:44 +0900

    잔고 변경 애니메이션 추가

--- components/TotalAssset.tsx
+++ components/TotalAssset.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { totalAssetState } from "@/app/recoilContextProvider";
-import Image from "next/image";
 import { useRecoilValue } from "recoil";
 import ReceiptLongIcon from "@mui/icons-material/ReceiptLong";
 import Link from "next/link";
+import AnimatedNumbers from "react-animated-numbers";
 
 function TotalAsset() {
   const totalAsset = useRecoilValue(totalAssetState);
@@ -13,8 +13,23 @@ function TotalAsset() {
     <div className="w-full text-center bg-gradient-to-t from-[#1428A0] to-[#2940c3] rounded-md sticky top-4 z-10 py-2 my-2">
       <div className="relative">
         <div className="font-normal text-white text-lg">잔고</div>
-        <div className="font-normal text-white text-md">
+        {/* <div className="font-normal text-white text-md">
           {Intl.NumberFormat("ko-KR").format(totalAsset)}
+        </div> */}
+        <div className="flex justify-center">
+          <AnimatedNumbers
+            includeComma
+            transitions={(index) => ({
+              type: "spring",
+              duration: index / 60,
+            })}
+            animateToNumber={totalAsset}
+            fontStyle={{
+              fontSize: 16,
+              fontWeight: "semibold",
+              color: "white",
+            }}
+          />
         </div>
         <div className="absolute top-0 right-0">
           <div className="p-2 pr-4">
