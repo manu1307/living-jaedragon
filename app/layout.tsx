927fd7000545790543c43e71272d1cebcaacee04
Sung Chul Park <jasonsc@naver.com>
2024-02-02 09:13:46 +0900

    구글 애널리틱스 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,15 +4,16 @@ import Script from "next/script";
 import "./globals.css";
 import RecoilContextProvider from "./recoilContextProvider";
 import Head from "next/head";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
-const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_MEASUREMENT_ID;
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_MEASUREMENT_ID!;
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://shopping-jaedragon.vercel.app/"),
   title: "재드래곤 몰래 쇼핑하기",
-  description:
-    "재드래곤의 카드가 우리 집 앞에 배송왔다고?",
+  description: "재드래곤의 카드가 우리 집 앞에 배송왔다고?",
   openGraph: {
     siteName: "재드래곤 몰래 쇼핑하기",
     images: [
@@ -37,27 +38,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${GA_MEASUREMENT_ID}', {
-            page_path: window.location.pathname,
-          });
-        `,
-          }}
-        />
-      </Head>
       <body className={inter.className}>
         <RecoilContextProvider>{children}</RecoilContextProvider>
       </body>
+      <GoogleAnalytics gaId={GA_MEASUREMENT_ID} />
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -9,10 +9,10 @@ export default function Home() {
   return (
     <main className="container min-h-screen md:p-16 p-2 pt-8 m-auto">
       <div className="flex flex-col items-center justify-center">
-        <div className="mt-3 mb-5 font-semibold text-2xl text-center">
+        <h2 className="mt-3 mb-5 font-semibold text-2xl text-center">
           재드래곤의 카드가 <br />
           배송 완료 되었습니다
-        </div>
+        </h2>
         <div className="relative w-32 h-32 bg-white rounded-full">
           <Image
             src={"/jaeDragon.png"}
@@ -21,9 +21,7 @@ export default function Home() {
             className="rounded-full"
           />
         </div>
-        <div className="my-3 font-semibold text-2xl">
-          재드래곤 몰래 쇼핑하기
-        </div>
+        <h1 className="my-3 font-semibold text-2xl">재드래곤 몰래 쇼핑하기</h1>
       </div>
 
       <TotalAsset />
